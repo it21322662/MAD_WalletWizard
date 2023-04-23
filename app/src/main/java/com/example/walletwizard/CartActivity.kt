472dd46d8db89ef0c9043d528a054354adcf73d0
IT21201882 <IT21201882@my.sliit.lk>
2023-04-24 01:01:58 +0530

    updated cart

--- app/src/main/java/com/example/walletwizard/CartActivity.kt
+++ app/src/main/java/com/example/walletwizard/CartActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 class CartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_cart)
     }
 }
