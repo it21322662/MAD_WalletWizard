90c71ecef419c69a619d8e6e913bafe6f781216a
Vimankith Perera <100081824+it21322662@users.noreply.github.com>
2023-05-03 10:18:20 +0530

    created new expenses layouts

---
+++ app/src/main/java/com/example/walletwizard/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.walletwizard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
