a1e817911be77a82df4ff2994268ef3af3ef2163
Vimankith Perera <100081824+it21322662@users.noreply.github.com>
2023-05-06 02:32:30 +0530

    Created Insert Expenses

---
+++ app/src/main/java/com/example/walletwizard/Models/expensesModel.kt
@@ -0,0 +1,8 @@
+package com.example.walletwizard.Models
+
+data class expensesModel(
+    var expensesId: String? = null,
+    var expensesAmount: String? = null,
+    var expensesNote: String? = null,
+    var expensesType: String? = null
+)
